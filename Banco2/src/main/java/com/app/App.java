ffbbf173b3e76c364956ef995ae94194a35aa941
JonasPequeno <jonaspdasilva31@gmail.com>
2018-06-07 17:35:32 -0300

    atividade jst

---
+++ Banco2/src/main/java/com/app/App.java
@@ -0,0 +1,14 @@
+package com.app;
+import com.view.Tela;
+
+/**
+ *
+ * @author jonaspeuqno
+ */
+public class App {
+    public static void main(String[] args) {
+        //Tela tela = new Tela();
+        //tela.setVisible(true);
+        
+    }
+}
